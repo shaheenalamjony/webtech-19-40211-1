626ea87c2545dd40fbb6609df1ef9fbdf9fe2242
shaheenalamjony <84887569+shaheenalamjony@users.noreply.github.com>
2021-08-08 21:21:36 +0600

    lab task 8 upload

---
+++ lab task 8/delete.php
@@ -0,0 +1,35 @@
+<?php include 'header.php'; 
+
+if(isset($_POST['deletebtn'])){
+
+    include 'config.php';  
+    $stu_id=$_POST['sid'];
+    $sql="DELETE from student where sid={$stu_id}";
+    $result=mysqli_query($conn,$sql) or die("Sql failed");
+    header("location:index.php");
+    mysqli_close($conn);
+    
+    
+}else{
+    
+
+    
+}
+
+
+?>
+
+
+<div id="main-content">
+    <h2>Delete Record</h2>
+    <form class="post-form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+        <div class="form-group">
+            <label>Id</label>            
+            <input type="text" name="sid" />            
+        </div>
+        <input class="submit" type="submit" name="deletebtn" value="Delete" />
+    </form>
+</div>
+</div>
+</body>
+</html>
